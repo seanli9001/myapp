ffa3e4fe3b67dd9a5c1413435d91dac9d443ccd0
Sean <seanli9001@163.com>
2018-06-13 16:45:32 +0800

    上报功能，以及确认值功能实现

--- src/App.js
+++ src/App.js
@@ -2,39 +2,66 @@ import 'antd/dist/antd.css'
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button, Input, Icon} from 'antd';
+import { Button, Input, DatePicker } from 'antd';
 
 const demoArray = [
-  {Name: '高压蒸汽', UOM:'吨', TagName:'4FIQ5002', Value:'124', ValueConfirm: '', Description: '高压蒸汽投入'},
-  {Name: '高压蒸汽2', UOM:'吨', TagName:'4FIQ5002', Value:'124', ValueConfirm: '', Description: '高压蒸汽投入'},
-  {Name: '高压蒸汽', UOM:'吨', TagName:'4FIQ5002', Value:'124', ValueConfirm: '', Description: '高压蒸汽投入'},
-  {Name: '高压蒸汽2', UOM:'吨', TagName:'4FIQ5002', Value:'124', ValueConfirm: '', Description: '高压蒸汽投入'},
-  {Name: '高压蒸汽', UOM:'吨', TagName:'4FIQ5002', Value:'124', ValueConfirm: '', Description: '高压蒸汽投入'},
-  {Name: '高压蒸汽2', UOM:'吨', TagName:'4FIQ5002', Value:'124', ValueConfirm: '', Description: '高压蒸汽投入'},
-  {Name: '高压蒸汽', UOM:'吨', TagName:'4FIQ5002', Value:'124', ValueConfirm: '', Description: '高压蒸汽投入'},
-  {Name: '高压蒸汽2', UOM:'吨', TagName:'4FIQ5002', Value:'124', ValueConfirm: '', Description: '高压蒸汽投入'},
-  {Name: '高压蒸汽', UOM:'吨', TagName:'4FIQ5002', Value:'124', ValueConfirm: '', Description: '高压蒸汽投入'},
-  {Name: '高压蒸汽2', UOM:'吨', TagName:'4FIQ5002', Value:'124', ValueConfirm: '', Description: '高压蒸汽投入'},
-  {Name: '高压蒸汽2', UOM:'吨', TagName:'4FIQ5002', Value:'124', ValueConfirm: '', Description: '高压蒸汽投入'}
+  { Name: '高压蒸汽', UOM: '吨', TagName: '4FIQ5002', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' },
+  { Name: '高压蒸汽2', UOM: '吨', TagName: '4FIQ5003', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' },
+  { Name: '高压蒸汽', UOM: '吨', TagName: '4FIQ5004', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' },
+  { Name: '高压蒸汽2', UOM: '吨', TagName: '4FIQ5005', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' },
+  { Name: '高压蒸汽', UOM: '吨', TagName: '4FIQ5006', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' },
+  { Name: '高压蒸汽2', UOM: '吨', TagName: '4FIQ5007', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' },
+  { Name: '高压蒸汽', UOM: '吨', TagName: '4FIQ5008', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' },
+  { Name: '高压蒸汽2', UOM: '吨', TagName: '4FIQ5009', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' },
+  { Name: '高压蒸汽', UOM: '吨', TagName: '4FIQ5010', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' },
+  { Name: '高压蒸汽2', UOM: '吨', TagName: '4FIQ5011', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' },
+  { Name: '高压蒸汽2', UOM: '吨', TagName: '4FIQ5012', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' }
 ]
+
 
 class App extends Component {
   state = {
     text: '',
-    demoArray: demoArray
+    demoArrayData: demoArray
   }
 
   handleClick = () => {
-    const {text} = this.state
+    const { text } = this.state
     if (text === 'ouch') {
       this.setState({
         text: 'yeah'
-      })  
+      })
     } else {
       this.setState({
         text: 'ouch'
       })
     }
+  }
+
+  dateChange = (date, dateString) => {
+    console.log(date, dateString);
+  }
+
+  handleChange = (event, index) => {
+    let finalValue = event.target.value
+
+    let demoArrayData = this.state.demoArrayData.slice()
+
+    demoArrayData[index].ValueConfirm = finalValue
+
+    this.setState({
+      demoArrayData: demoArrayData
+    })
+
+  }
+
+  handleSubmit = () => {
+    let data = this.state.demoArrayData.map(item => {
+      return item.ValueConfirm * 1
+    })
+
+    //修改确认值后，确认值以数组的形式显示、或者回传。
+    console.log(data)
   }
 
   render() {
@@ -53,42 +80,42 @@ class App extends Component {
           <div className="content">
             <div className="header">
               <div className="headerleft">
-                <span className="text">日期：</span><Input placeholder="选择日期" type="text" addonAfter={<Icon type="calendar" />} />
+                <span className="text">日期：</span><DatePicker onChange={this.dateChange} placeholder="选择日期" />
                 <Button type={'primary'} /* onClick={this.handleClick }*/>查询</Button>
               </div>
               <div className="headerright">
-              <Button type={'primary'} /* onClick={this.handleClick }*/>上报</Button>
+                <Button type={'primary'} onClick={this.handleSubmit }>上报</Button>
               </div>
             </div>
-          <div className="tablecontent">
-            <table>
-              <thead>
-                <tr>
-                  <th>项目</th>
-                  <th>位号</th>
-                  <th>单位</th>
-                  <th>原始值</th>
-                  <th>确认值</th>
-                  <th>备注</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.demoArray.map((data, index)=> (
-                  <tr key={index}>
-                    <td>{data.Name}</td>
-                    <td>{data.UOM}</td>
-                    <td>{data.TagName}</td>
-                    <td>{data.Value}</td>
-                    <td>{data.ValueConfirm}</td>
-                    <td>{data.Description}</td>
+            <div className="tablecontent">
+              <table>
+                <thead>
+                  <tr>
+                    <th>项目</th>
+                    <th>位号</th>
+                    <th>单位</th>
+                    <th>原始值</th>
+                    <th>确认值</th>
+                    <th>备注</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {this.state.demoArrayData.map((data, index) => (
+                    <tr key={index}>
+                      <td>{data.Name}</td>
+                      <td>{data.UOM}</td>
+                      <td>{data.TagName}</td>
+                      <td>{data.Value}</td>
+                      <td><Input type="text" value={data.ValueConfirm} onChange={(ev) => this.handleChange(ev, index)} /></td>
+                      <td>{data.Description}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     );
   }
 }
