86f6ccb3d8a409ab8ecc853278ef8824ef5e0468
Sean <seanli9001@163.com>
2018-06-20 18:28:56 +0800

    final

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import 'antd/dist/antd.css'
 import React, { Component } from 'react';
 import './App.css';
 import { Button, Input, DatePicker, Table } from 'antd';
-//import TableEB from './Tables/tableEB';
-//import logo from './logo.svg';
-//import Game from './Game/Game';
 
 const data = [
   { Name: '高压蒸汽', UOM: '吨', TagName: '4FIQ5002', Value: 124, ValueConfirm: 124, Description: '高压蒸汽投入' },
@@ -28,7 +25,6 @@ class App extends Component {
   }
 
   dateChange = (date, dateString) => {
-    console.log(date, dateString);
     this.setState({
       dateStr: (date, dateString)
     })
@@ -50,15 +46,10 @@ class App extends Component {
       data: data
     })
   }
-
-  //elements.map(item => {return item.ValueConfirm}) 与 
-  //elements.map(function(item){return item.ValueConfirm})一样
   handleSubmit = () => {
     let submitConfirmValue = this.state.data.map(item => {
       return item.ValueConfirm * 1
     })
-    //修改确认值后，确认值以数组的形式显示、或者回传。
-    //console.log(data)
     console.log(submitConfirmValue);
   }
   constructor(props) {
@@ -82,15 +73,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">fuck off sdasdasdasd</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button type={'primary'} onClick={this.handleClick}>hit me</Button>
-        <h4>{this.state.text}</h4> */}
         <div className="box">
           <div className="content">
             <div className="header">
@@ -105,141 +87,11 @@ class App extends Component {
             <div className="table">
               <Table columns={this.columns} dataSource={this.state.data} pagination={{ pageSize: 12 }} rowKey={record => record.TagName} scroll={{ x: 1024 }} />
             </div>
-            {/* <div className="tablecontent">
-              <table>
-                <thead>
-                  <tr>
-                    <th>项目</th>
-                    <th>位号</th>
-                    <th>单位</th>
-                    <th>原始值</th>
-                    <th>确认值</th>
-                    <th>备注</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.state.demoArrayData.map((data, index) => (
-                    <tr key={index}>
-                      <td>{data.Name}</td>
-                      <td>{data.UOM}</td>
-                      <td>{data.TagName}</td>
-                      <td>{data.Value}</td>
-                      <td><Input type="text" value={data.ValueConfirm} onChange={(ev) => this.handleChange(ev, index)} /></td>
-                      <td>{data.Description}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div> */}
-            {/* <TableEB /> */}
           </div>
         </div>
-        {/*<Game />*/}
       </div>
     );
   }
 }
 
 export default App;
-
-
-  // test = (_selectedElements, _searchedElements) => {
-  //   {
-  //     const selectedElements = [{ WebId: 'a', Timestamp: 'a' }, { WebId: 'b' }]
-  //     const searchedElements = [{ WebId: 'a', Timestamp: 'a' }, { WebId: 'c' }]
-
-  //     let flag = false;
-  //     let isExsit = []
-  //     //let result = newSelectedElements.concat(newSearchedElements)
-
-  //     // function selectedByWebId(value) {
-  //     //   return item => value === item.WebId
-  //     // }
-
-  //     // function selectedByWebId(searchedElementItemWebId) {
-  //     //   return function (selectedElementItem) {
-  //     //     return selectedElementItem.WebId === searchedElementItemWebId
-  //     //   }
-  //     // }
-  //     if (selectedElements==false) {
-  //       flag=false;
-  //     }
-  //     else{
-  //       isExsit=searchedElements.map(item => {
-  //         let idx = _findIndex(selectedElements, {WebId: item.WebId})
-  //         return idx
-  //       })
-  //       // flag = (isExsit.length === searchedElements.length)
-
-  //       flag = (isExsit.indexOf(-1) === -1)
-  //     }
-  //     console.log(flag);
-  //   }
-  // }
-  // // test = (_selectedElements, _searchedElements) => {
-  // //   const selectedElements = []
-  // //   const searchedElements = []
-
-  // //   let flag = false;
-  // //   let isExsit = []
-  // //   let result = selectedElements.concat(searchedElements)
-
-  // //   isExsit = searchedElements.map(item => {
-  // //     let idx = _findIndex(selectedElements, item)
-  // //     return idx
-  // //   })
-
-  // //   result==false
-  // //     ? (flag = false)
-  // //     : (isExsit.indexOf(-1) === -1)
-  // //       ? (flag = true)
-  // //       : flag;
-
-  // //   console.log(flag);
-  // // }
-
-
-  // test1 = (_selectedElements, _searchedElements, isCheckAll=true) => {
-  //     let selectedElements = [{WebId: 'a'}, {WebId: 'b'}]
-  //     let searchedElements = [{WebId: 'b'}, {WebId: 'c'}]
-  //     const newConcatArray=[];
-
-
-  //     if (isCheckAll) {
-  //       let concatArray = [...selectedElements, ...searchedElements];
-  //       // let objConcatArray=concatArray.map(item=>item.WebId);
-  //       concatArray.forEach(item=>{
-  //         if (!newConcatArray.find(item1 => item.WebId===item1.WebId)) {
-  //           newConcatArray.push(item)
-  //         }
-  //       });
-  //     } else {
-  //       selectedElements.forEach(item => {
-  //         if(searchedElements.find(item1 => item.WebId === item1.WebId)) {
-  //           newConcatArray.push(item)
-  //         }
-  //       })
-  //     }
-  //     return newConcatArray
-  //     // else {
-  //     //   let objSelected = newSelectedElements.map(item => item.WebId);
-  //     //   let objSearched = newSearchedElements.map(item => item.WebId);
-  //     //   let newObjSelected = []
-  //     //   for (let i = 0; i < newSearchedElements.length; i++) {
-  //     //     isExsit.push(objSelected.indexOf(objSearched[i]))
-  //     //   }
-
-  //     //   for (let j = isExsit.length - 1; j >= 0; j--) {
-  //     //     let index = isExsit[j];
-  //     //     if(index != -1){
-  //     //       newObjSelected.push(objSelected[index])
-  //     //     }
-  //     //     newFinalArray = newObjSelected.map(item => {
-  //     //       return {
-  //     //         WebId:item
-  //     //       }
-  //     //     })
-  //     //   }
-  //     // }
-  //     //return [...selectedElements]
-  // }
